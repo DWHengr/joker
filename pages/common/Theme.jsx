ce2f454a634e4bf46a65c8777f5006d4bb9451f0
Heath <duweihengmr@163.com>
2023-08-12 18:34:01 +0800

    fix: add computational niuniu function

--- pages/common/Theme.jsx
+++ pages/common/Theme.jsx
@@ -1,7 +1,12 @@
 const primary = "#441188";
+const secondary = "#8a8a8a";
+const minor = "#173757";
+
 
 export const theme = {
     primary: primary,
+    minor: minor,
+    secondary: secondary,
     containerBackgroundColor: "#efefef",
-    tabTxtColor: "#8a8a8a",
+    tabTxtColor: secondary,
 }
--- pages/douniu/Douniu.tsx
+++ pages/douniu/Douniu.tsx
@@ -1,15 +1,170 @@
-import {StyleSheet, ScrollView, Text} from 'react-native'
+import {StyleSheet, ScrollView, Text, View, TouchableOpacity, Image} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
 import CustomHeaderReturn from "../../component/CustomHeaderReturn";
 import {theme} from "../common/Theme";
+import {Button} from "@rneui/themed";
+import {useState} from "react";
+import PokerSegmentPicker from "../../component/PokerSegmentPicker";
+import {LinearGradient} from 'expo-linear-gradient';
+
+const cardsImg =
+    {
+        heart: require('../../assets/poker-heart.png'),
+        diamond: require('../../assets/poker-diamond.png'),
+        club: require('../../assets/poker-club.png'),
+        spade: require('../../assets/poker-spade.png')
+    }
 
 export default function Douniu() {
+    const [pokerVisible, setPokerVisible] = useState(false);
+    const [currentSelectionCardIndex, setCurrentSelectionCardIndex] = useState(0);
+    const [cards, setCards] = useState([
+        {num: "A", suit: "heart"},
+        {num: "K", suit: "club"},
+        {num: "J", suit: "diamond"},
+        {num: "Q", suit: "spade"},
+        {num: "8", suit: "heart"},
+    ])
+
+    const onSelectCardAndNum = async (cardIndex) => {
+        await setCurrentSelectionCardIndex(cardIndex);
+        setPokerVisible(true);
+    }
     return (
         <SafeAreaView style={[styles.container]}>
+            <PokerSegmentPicker
+                visible={pokerVisible}
+                onCancel={() => setPokerVisible(false)}
+                onConfirm={(value) => {
+                    cards[currentSelectionCardIndex].suit = value.suit;
+                    cards[currentSelectionCardIndex].num = value.num;
+                    setCards(cards)
+                    setPokerVisible(false)
+                }}
+                defaultSelections={cards[currentSelectionCardIndex]}
+            />
             <CustomHeaderReturn title='斗牛' isReturn={true}></CustomHeaderReturn>
-            <ScrollView style={{backgroundColor: '#ffffff'}}>
-                <Text>斗牛</Text>
-            </ScrollView>
+            <View style={{backgroundColor: '#ffffff'}}>
+                <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 15}}>
+                    <View style={{height: 130, width: '100%', flexDirection: 'colum',}}>
+                        <View style={{flex: 1, flexDirection: 'colum'}}>
+                            <View style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                paddingRight: 20,
+                                paddingLeft: 20
+                            }}>
+                                {cards?.map((card, index) => (
+                                    <TouchableOpacity key={index} onPress={() => onSelectCardAndNum(index)}>
+                                        <View style={[styles.cardsContainer]}>
+                                            <Image
+                                                style={[StyleSheet.absoluteFill, styles.cardsImg]}
+                                                source={cardsImg[card.suit]}
+                                            />
+                                            <View style={[styles.cardsTextContainer]}>
+                                                <Text style={{fontSize: 16}}>{card.num}</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableOpacity>
+                                ))}
+                            </View>
+                            <View style={{
+                                flexDirection: 'colum',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <View style={{width: 200}}>
+                                    <Button
+                                        ViewComponent={LinearGradient}
+                                        linearGradientProps={{
+                                            colors: [theme.secondary, theme.minor],
+                                            start: {x: 0, y: 0.5},
+                                            end: {x: 1, y: 0.5},
+                                        }}
+                                        buttonStyle={{
+                                            borderRadius: 15,
+                                            backgroundColor: theme.minor,
+                                        }}
+                                    >
+                                        <Text style={{color: '#ffffff'}}>计算牛牛</Text>
+                                    </Button>
+                                </View>
+                            </View>
+                            <View style={{
+                                flexDirection: 'colum',
+                                justifyContent: 'center',
+                                alignItems: 'center'
+                            }}>
+                                <Text>牛2</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <ScrollView style={{height: '100%'}}>
+                    <View>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                        <Text>222</Text>
+                    </View>
+                </ScrollView>
+            </View>
         </SafeAreaView>
     )
 }
@@ -17,6 +172,24 @@ export default function Douniu() {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: theme.containerBackgroundColor,
-        flex: 1
+        flex: 1,
     },
+    cardsContainer: {
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    cardsImg: {
+        width: 60,
+        height: 60
+    },
+    cardsTextContainer: {
+        width: 60,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+
 });
