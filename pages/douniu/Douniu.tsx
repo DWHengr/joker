a9446759dbb06f64dc0bdc6201625f1f744e91b4
Heath <duweihengmr@163.com>
2023-08-14 14:25:18 +0800

    fix: perfect member list

--- pages/douniu/Douniu.tsx
+++ pages/douniu/Douniu.tsx
@@ -1,11 +1,12 @@
-import {StyleSheet, ScrollView, Text, View, TouchableOpacity, Image} from 'react-native'
+import {StyleSheet, ScrollView, Text, View, TouchableOpacity, Image, TextInput} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
 import CustomHeaderReturn from "../../component/CustomHeaderReturn";
 import {theme} from "../common/Theme";
-import {Button, Input} from "@rneui/themed";
+import {Button} from "@rneui/themed";
 import {useState} from "react";
 import PokerSegmentPicker from "../../component/PokerSegmentPicker";
 import {LinearGradient} from 'expo-linear-gradient';
+
 
 const cardsImg =
     {
@@ -21,52 +22,76 @@ const CardValues = {
 
 const userInfos = [
     {
+        id: '1',
         name: 'Heath',
         score: 100,
         portrait: '',
         status: '正在结算中...',
+        isDealers: true,
+        isOwner: true,
     },
     {
+        id: '2',
         name: 'Heath2',
         score: 100,
         portrait: '',
         status: '牛2',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '3',
         name: 'Heath3',
         score: -20,
         portrait: '',
         status: '正在结算中...',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '4',
         name: 'Heath4',
         score: 100,
         portrait: '',
         status: '没牛',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '5',
         name: 'Heath5',
         score: 100,
         portrait: '',
         status: '正在结算中...',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '6',
         name: 'Heath5',
         score: 100,
         portrait: '',
         status: '正在结算中...',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '7',
         name: 'Heath5',
         score: 100,
         portrait: '',
         status: '正在结算中...',
+        isDealers: false,
+        isOwner: false,
     },
     {
+        id: '8',
         name: 'Heath5',
         score: 100,
         portrait: '',
         status: '正在结算中...',
+        isDealers: false,
+        isOwner: false,
     },
 ];
 
@@ -75,6 +100,7 @@ export default function Douniu() {
     const [pokerVisible, setPokerVisible] = useState(false);
     const [currentSelectionCardIndex, setCurrentSelectionCardIndex] = useState(0);
     const [niuResult, setNiuResult] = useState(-1);
+    const [scoreResult, setScoreResult] = useState(-1);
     const [cards, setCards] = useState([
         {num: "A", suit: "heart"},
         {num: "K", suit: "club"},
@@ -192,11 +218,100 @@ export default function Douniu() {
                     <View>
                         <Text>房间号：10888</Text>
                     </View>
+                    <View style={{flexDirection: 'row', height: 50, justifyContent: 'center', alignItems: 'center'}}>
+                        <Button
+                            size='sm'
+                            ViewComponent={LinearGradient}
+                            linearGradientProps={{
+                                colors: [theme.secondary, theme.minor],
+                                start: {x: 0, y: 0.5},
+                                end: {x: 1, y: 0.5},
+                            }}
+                            buttonStyle={{
+                                width: 30,
+                                borderRadius: 5,
+                                backgroundColor: theme.minor,
+                            }}
+                        >
+                            <Text style={{color: '#ffffff'}}>-</Text>
+                        </Button>
+                        <View style={{
+                            width: 40
+                        }}>
+                            <TextInput
+                                style={{
+                                    color: theme.secondary,
+                                    borderRadius: 4,
+                                    paddingLeft: 2,
+                                    paddingRight: 2,
+                                    backgroundColor: theme.minor,
+                                }}
+                                keyboardType="numeric"
+                                value={scoreResult}
+                                placeholder="得分"
+                                placeholderTextColor={theme.secondary}
+                                onChangeText={(text) => setScoreResult(text.replace(/[^-0-9]/g, ''))}
+                            ></TextInput>
+                        </View>
+                        <Button
+                            size='sm'
+                            ViewComponent={LinearGradient}
+                            linearGradientProps={{
+                                colors: [theme.secondary, theme.minor],
+                                end: {x: 0, y: 0.5},
+                                start: {x: 1, y: 0.5},
+                            }}
+                            buttonStyle={{
+                                width: 30,
+                                borderRadius: 5,
+                                backgroundColor: theme.minor,
+                            }}
+                        >
+                            <Text style={{color: '#ffffff'}}>+</Text>
+                        </Button>
+                        <Button
+                            size='sm'
+                            onPress={onCalculateNiu}
+                            ViewComponent={LinearGradient}
+                            linearGradientProps={{
+                                colors: [theme.secondary, theme.minor],
+                                start: {x: 0, y: 0.5},
+                                end: {x: 1, y: 0.5},
+                            }}
+                            buttonStyle={{
+                                width: 90,
+                                borderRadius: 5,
+                                marginLeft: 2,
+                                backgroundColor: theme.minor,
+                            }}
+                        >
+                            <Text style={{color: '#ffffff'}}>提交</Text>
+                        </Button>
+                        <Button
+                            size='sm'
+                            onPress={onCalculateNiu}
+                            ViewComponent={LinearGradient}
+                            linearGradientProps={{
+                                colors: [theme.secondary, theme.minor],
+                                end: {x: 0, y: 0.5},
+                                start: {x: 1, y: 0.5},
+                            }}
+                            buttonStyle={{
+                                width: 90,
+                                borderRadius: 5,
+                                marginLeft: 2,
+                                backgroundColor: theme.minor,
+                            }}
+                        >
+                            <Text style={{color: '#ffffff'}}>准备</Text>
+                        </Button>
+                    </View>
                     <ScrollView style={{height: '70%', width: '100%',}}>
                         <View>
                             {
                                 userInfos?.map(info => (
                                     <View
+                                        key={info.id}
                                         style={[styles.userInfoContainer]}>
                                         <View style={{flexDirection: 'row', alignItems: 'center'}}>
                                             <View style={{width: 50, height: 50, borderRadius: 10}}>
@@ -207,7 +322,27 @@ export default function Douniu() {
                                                 </Image>
                                             </View>
                                             <View style={{marginLeft: 6}}>
-                                                <Text style={{color: theme.secondary}}>{info.name}</Text>
+                                                <View style={{flexDirection: 'row'}}>
+                                                    <Text style={{color: theme.secondary}}>{info.name}</Text>
+                                                    {
+                                                        info.isOwner ? <Image
+                                                            style={{
+                                                                height: 20,
+                                                                width: 20
+                                                            }}
+                                                            source={require('../../assets/owner.png')}
+                                                        /> : <></>
+                                                    }
+                                                    {
+                                                        info.isDealers ? <Image
+                                                            style={{
+                                                                height: 20,
+                                                                width: 20
+                                                            }}
+                                                            source={require('../../assets/crown.png')}
+                                                        /> : <></>
+                                                    }
+                                                </View>
                                                 <Text style={{
                                                     fontSize: 16,
                                                     marginTop: 5,
@@ -216,7 +351,8 @@ export default function Douniu() {
                                                 }}>{info.score}</Text>
                                             </View>
                                         </View>
-                                        <View><Text>正在结算中...</Text></View>
+                                        <View
+                                            style={{width: 100, alignItems: 'center'}}><Text>{info.status}</Text></View>
                                     </View>
                                 ))
                             }
@@ -264,9 +400,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         height: 80,
         borderBottomWidth: 1,
-        borderBottomColor: theme.secondary,
+        borderBottomColor: theme.containerBackgroundColor,
         padding: 6,
         borderRadius: 12,
         backgroundColor: "#ffffff"
-    }
+    },
 });
