fe07eafd40c3627856ea4fc030b9620946f0a712
Heath <duweihengmr@163.com>
2023-08-14 20:10:39 +0800

    feat: add GradualButton component

---
+++ component/GradualButton.tsx
@@ -0,0 +1,27 @@
+import {Button} from "@rneui/themed";
+import {LinearGradient} from "expo-linear-gradient";
+import {theme} from "../pages/common/Theme";
+import {Text} from "react-native";
+
+export default function GradualButton({size, text, buttonStyle, isPros, onPress}) {
+    return (
+        <Button
+            onPress={() => {
+                if (onPress) onPress()
+            }}
+            size={size}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+                colors: [theme.secondary, theme.minor],
+                end: isPros ? {x: 0, y: 0.5} : {x: 1, y: 0.5},
+                start: isPros ? {x: 1, y: 0.5} : {x: 0, y: 0.5},
+            }}
+            buttonStyle={{
+                ...buttonStyle,
+                backgroundColor: theme.minor,
+            }}
+        >
+            <Text style={{color: '#ffffff'}}>{text}</Text>
+        </Button>
+    )
+}
