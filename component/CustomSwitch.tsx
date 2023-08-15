9951c994dd8b60c6e48f9c35ec027f6842f0f2bc
Heath <duweihengmr@163.com>
2023-08-15 17:51:13 +0800

    feat: add CustomSwitch component

---
+++ component/CustomSwitch.tsx
@@ -0,0 +1,22 @@
+import {Switch, View, Text} from "react-native";
+import {theme} from "../pages/common/Theme";
+
+export default function CustomSwitch({text, enabled, onValueChange}) {
+    return (
+        <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
+            <Text style={{color: theme.minor}}>{text}</Text>
+            <Switch
+                style={{height: 36}}
+                trackColor={{false: theme.secondary, true: theme.secondary}}
+                thumbColor={enabled ? theme.minor : "#f4f3f4"}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={
+                    (value) => {
+                        if (onValueChange) onValueChange(value);
+                    }
+                }
+                value={enabled}
+            />
+        </View>
+    )
+}
