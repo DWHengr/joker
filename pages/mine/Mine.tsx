2f12f4c5dec29171e2a4ced87eae0f521b1c3e47
Heath <duweihengmr@163.com>
2023-08-17 20:32:00 +0800

    fix: perfect Mine page

--- pages/mine/Mine.tsx
+++ pages/mine/Mine.tsx
@@ -1,19 +1,39 @@
-import {ScrollView, StyleSheet, Text} from 'react-native'
+import {Image, StyleSheet, Text, View} from 'react-native'
 import {SafeAreaView} from "react-native-safe-area-context";
+import {theme} from "../common/Theme";
 
 export default function Mine() {
     return (
-        <ScrollView>
+        <View style={{alignItems: 'center'}}>
+            <View style={{width: '100%', height: 200}}>
+                <Image style={
+                    {
+                        width: '100%',
+                        height: 200,
+                        borderBottomLeftRadius: 30,
+                        borderBottomRightRadius: 30
+                    }}
+                       source={require('../../assets/bg2.png')}/>
+            </View>
+            <View style={{
+                width: '80%',
+                height: 140,
+                backgroundColor: '#ffffff',
+                borderRadius: 20,
+                marginTop: -70
+            }}>
+
+            </View>
             <SafeAreaView style={[styles.container]}>
                 <Text>Mine</Text>
             </SafeAreaView>
-        </ScrollView>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: 'center',
+        alignItems: theme.containerBackgroundColor,
         paddingLeft: 32,
         paddingRight: 32
     },
