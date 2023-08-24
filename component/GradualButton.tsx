57b4fe6f403a9b9d9849407d38a9e11fae68efd8
Heath <duweihengmr@163.com>
2023-08-24 13:53:31 +0800

    fix: modify GradualButton component

--- component/GradualButton.tsx
+++ component/GradualButton.tsx
@@ -3,16 +3,17 @@ import {LinearGradient} from "expo-linear-gradient";
 import {theme} from "../pages/common/Theme";
 import {Text} from "react-native";
 
-export default function GradualButton({size, text, buttonStyle, isPros, onPress}) {
+export default function GradualButton({size, text, buttonStyle, isPros, disabled = false, onPress}) {
     return (
         <Button
+            disabled={disabled}
             onPress={() => {
                 if (onPress) onPress()
             }}
             size={size}
             ViewComponent={LinearGradient}
             linearGradientProps={{
-                colors: [theme.secondary, theme.primary],
+                colors: [theme.secondary, disabled ? theme.secondary : theme.primary],
                 end: isPros ? {x: 0, y: 0.5} : {x: 1, y: 0.5},
                 start: isPros ? {x: 1, y: 0.5} : {x: 0, y: 0.5},
             }}
