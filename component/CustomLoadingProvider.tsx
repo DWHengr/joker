de41f7112e4c1194bcc6dc505b0a330936581363
Heath <duweihengmr@163.com>
2023-09-05 17:30:48 +0800

    feat: add CustomLoadingProvider component

---
+++ component/CustomLoadingProvider.tsx
@@ -0,0 +1,50 @@
+import React, {createContext, useCallback, useContext, useState} from "react";
+import {View, StyleSheet} from "react-native";
+import Spinner from 'react-native-loading-spinner-overlay';
+
+const LoadingBarContext = createContext();
+
+export function useLoading() {
+    const context = useContext(LoadingBarContext);
+    return context;
+}
+
+export default function CustomLoadingProvider({children}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [tip, setTip] = useState("");
+
+    const showLoading = useCallback((tip = "正在加载中") => {
+        setTip(tip);
+        setIsLoading(true);
+    }, []);
+
+    const hideLoading = useCallback(() => {
+        setIsLoading(false);
+    }, []);
+
+    return (
+        <LoadingBarContext.Provider value={{isLoading, showLoading, hideLoading}}>
+            <View>
+                {isLoading && (
+                    <View>
+                        <Spinner
+                            animation="fade"
+                            overlayColor="rgba(0, 0, 0, 0.7)"
+                            visible={isLoading}
+                            textContent={tip}
+                            textStyle={[styles.text]}
+                        />
+                    </View>
+                )}
+            </View>
+            {children}
+        </LoadingBarContext.Provider>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 16,
+        color: "#fff"
+    }
+})
